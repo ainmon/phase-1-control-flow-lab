5e957b22360f3d893bfa293834a7fac3fdb9af44
Adam <inmon.adam@yahoo.com>
2021-09-20 21:54:22 -0500

    commit tree a4a0467451c0f2c52af2efd6d552c5462aac8dcd

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  if (distance <= 500){
+    return 'This one is on me!';
+  }
+  else if (distance <= 2500){
+    return 'I will gladly take your thirty bucks.';
+  }
+  else if(distance > 2500){
+    return 'No can do.';
+  }
+} 
+
+function ternaryCheckCity(city){
+  const rightCity = (city !== 'NYC') ? 'No go.' : 'Ok, sounds good.';
+  return rightCity;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip){
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+  switch(tip){
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.';
+  }
 }
